c8bcfe5774fcfe87438233c3532821407f15f978
Denis <apekindenis@gmail.com>
2017-05-18 09:36:44 +0300

    less5-6

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,13 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Page;
+use App\Service;
+use App\People;
+use App\Portfolio;
 
 class IndexController extends Controller
 {
     //
     public function execute(Request $request){
+        $pages = Page::all();
+        $portfolios = Portfolio::get(['name','filter','images']);
+        $services = Service::where('id','<',20)->get();
+        $peoples = People::take(3)->get();
 
-        return view('layouts.site');
+        $menu = [];
+        foreach ($pages as $page){
+            $item = ['title'=>$page->name,'alias'=>$page->alias];
+            array_push($menu, $item);
+        }
+        $item=['title'=> 'Services','alias'=>'service'];
+        array_push($menu, $item);
+        $item=['title'=> 'Portfolio','alias'=>'Portfolio'];
+        array_push($menu, $item);
+        $item=['title'=> 'Team','alias'=>'team'];
+        array_push($menu, $item);
+        $item=['title'=> 'Contact','alias'=>'contact'];
+        array_push($menu, $item);
+
+        return view('site.index', [
+            'menu'       => $menu,
+            'pages'      => $pages,
+            'portfolios' => $portfolios,
+            'services'   => $services,
+            'peoples'    => $peoples,
+        ]);
 
     }
 }
