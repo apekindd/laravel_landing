ab66f1ae5de018f55c4b4923e077f480e6bdbd22
Denis <apekindenis@gmail.com>
2017-05-19 09:10:13 +0300

    less8

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,11 +8,37 @@ use App\Service;
 use App\People;
 use App\Portfolio;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 
 class IndexController extends Controller
 {
     //
     public function execute(Request $request){
+
+        if($request->isMethod('post')){
+            $messages = [
+                'required' => 'Обязательное поле :attribute',
+                'email'    => 'Поле :attribute должно соответсвовать email адресу',
+            ];
+            $this->validate($request, [
+                'name'  => 'required|max:255',
+                'email' => 'required|email',
+                'text'  => 'required',
+            ], $messages);
+
+            $data = $request->all();
+
+            $result = Mail::send('site.email',['data'=>$data], function($message) use ($data){
+                $mail_admin = env('MAIL_ADMIN');
+                $message->from($data['email'], $data['name']);
+                $message->to($mail_admin,'Mr.Sam')->subject('Question');
+            });
+
+            if($request){
+                return redirect()->route('home')->with('status','Email is send');
+            }
+        }
+
         $pages = Page::all();
         $portfolios = Portfolio::get(['name','filter','images']);
         $services = Service::where('id','<',20)->get();
